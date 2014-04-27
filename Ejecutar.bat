b2b0be9ea936ed42f9e314b5bdc3e2cb15f445e8
Davidx157 <davidx1578@gmail.com>
2014-04-27 16:39:12 -0500

    Archivo Ejecutar.bat creado.

---
+++ Ejecutar.bat
@@ -0,0 +1,36 @@
+-.-
+PixelKernel A
+MetaKernel: CraftBukkit V "1.7.2 R0.3"
+MetaFile created: 'Ejecutar.bat'
+File Type: 'Windows MSDOS executable File'
+PixelGit Status: 'OK'
+Base gk: 'MS-DOS'
+Use lib: 'Windows CMD'
+File meta: 'Java Loader MSDOS Based'
+
+SourceCode:
+@echo off
+color 03
+echo.
+echo PixelTeK Loader V1.3
+echo. 
+echo Bienvenido a el Bukkit Launcher de PixelWarZ.
+echo El JAR Basico con nombre "PixelBukkit.jar" Sera iniciado en breve.
+echo. 
+PAUSE
+echo.
+echo ----------------------------------------------------------------
+echo.
+echo Ok. El JAR Se iniciara en breve. Ten paciencia.
+echo Se esta inciando el jar basico...
+echo Cargando Librerias basicas...
+echo Pre iniciando Java...
+color 02
+echo Inicio correcto!
+java -Xmx3024M -jar PixelBukkit.jar -o false
+echo.
+echo.
+echo Sistemas de Loaders PixelTeK y kernel bukkit apagados.
+color 0c
+
+PAUSE
